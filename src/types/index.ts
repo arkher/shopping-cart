dbb57e808505ccae5610c33b311d7fb4d42343a1
Paulo Gomes <paulo.rgomes22@gmail.com>
2025-07-06 21:52:59 -0300

    chore: typing

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  description: string;
+  category: string;
+}
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
+}
+
+export interface CartContextType {
+  items: CartItem[];
+  addToCart: (product: Product) => void;
+  removeFromCart: (productId: number) => void;
+  updateQuantity: (productId: number, quantity: number) => void;
+  clearCart: () => void;
+  getTotalItems: () => number;
+  getTotalPrice: () => number;
+} 
